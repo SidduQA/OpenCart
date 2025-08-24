46547a62843e22e9488ee36e201a45438120f63f
SidduQA <siddarth2756@gmail.com>
2025-08-24 11:09:22 +0530

    Removed TC001 Prioroty

--- src/testCases/TC001_AccountLogin.java
+++ src/testCases/TC001_AccountLogin.java
@@ -11,7 +11,7 @@ public class TC001_AccountLogin extends baseClass {
 	HomePage hp;
 	LoginPage lp;
 
-	@Test(groups = { "regression", "master" }, priority = 0)
+	@Test(groups = { "regression", "master" })
 	public void verifyLogin() {
 		hp = new HomePage(driver);
 
